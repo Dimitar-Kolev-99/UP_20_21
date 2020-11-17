8ac2f035f60cd8bbdd93029fe19f97e1d295d29e
Dimitar-Kolev-99 <dimitarrukolev@gmail.com>
2020-11-17 19:23:32 +0200

    Moved to solutions folder

---
+++ Week7/Solutions/Task1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+static int calculator(int firstNumber, int secondNumber, char operation) {
+
+	int answer;
+
+	switch (operation)
+	{
+	case '+':
+		answer = firstNumber + secondNumber;
+		break;
+	case '-':
+		answer = firstNumber - secondNumber;
+		break;
+	case '*':
+		answer = firstNumber * secondNumber;
+		break;
+	case '/':
+		answer = firstNumber / secondNumber;
+		break;
+	case '%':
+		answer = firstNumber % secondNumber;
+		break;
+	default:
+		return -1;
+	}
+
+	return answer;
+}
---
+++ Week7/Solutions/Task2.cpp
@@ -0,0 +1,3 @@
+static bool checkIfPointIsInSquare(double x, double y) {
+	return (x <= 6 && x >= -6) && (y <= 6 && y >= -6);
+}
